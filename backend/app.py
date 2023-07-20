610791a6508f0041c8a5f0f6af2daa96a107a643
NeerajHazarika <neerajprataphazarika@gmail.com>
2023-07-20 18:34:13 +0530

    updated app.py to remove local logs

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,9 @@
-import logging
-from flask import Flask 
+from flask import Flask
 from frontend import frontend_bp
 from api import api_bp
 from config import app_config
 from dotenv import load_dotenv
-import argparse
 
-# Load environment variables from .env
 load_dotenv()
 
 def create_app(config_name):
@@ -15,20 +12,6 @@ def create_app(config_name):
     # Set the app configuration based on the provided config name
     app.config.from_object(app_config[config_name])
 
-    # Add the handler to the Flask app's logger
-    app.logger.setLevel(logging.INFO)
-
-    # Create a file handler for the log file
-    file_handler = logging.FileHandler('app.log')
-    file_handler.setLevel(logging.INFO)
-
-    # Create a log formatter and add it to the file handler
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-
-    # Add the file handler to the logger
-    app.logger.addHandler(file_handler)
-
     app.register_blueprint(frontend_bp)
     app.register_blueprint(api_bp)
     
@@ -36,11 +19,6 @@ def create_app(config_name):
 
 
 if __name__ == '__main__':
-    # take app env as a cmdline argument (dev or prod) with --env flag
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--env', help='Environment')
-    # args = parser.parse_args()
-    # app_env = args.env
     app = create_app('development')
 
     app.run(host='0.0.0.0', port=5000)
