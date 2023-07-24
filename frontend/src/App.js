ca58335478545a325094a517b46f5fafc5ab3933
NeerajHazarika <neerajprataphazarika@gmail.com>
2023-07-24 17:04:55 +0530

    added routes, updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import InstructionPage from "./pages/Instruction/Instruction.jsx";
+import HomePage from "./pages/Home/Home.jsx";
+import TestPage from "./pages/Test/Test.jsx"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<InstructionPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/instruction" element={<InstructionPage />} />
+        <Route
+          path="/test/:testID/attempt/:attemptID/question/:questionNo"
+          element={<TestPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
