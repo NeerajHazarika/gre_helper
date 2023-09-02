3762d1c2617a3f71817bc4f431211301ec3b82bc
a-nnurag <asc15.lko@gmail.com>
2023-09-02 23:23:34 +0530

    3mmcq made

---
+++ frontend/src/pages/3mmcq/Desktop23mmcq.js
@@ -0,0 +1,487 @@
+import "./Desktop23mmcq.css";
+
+const Desktop23mmcq = () => {
+
+  return (
+    <div className="desktop-2-3mmcq">
+      <div className="desktop-2-3mmcq-child" />
+      <div className="desktop-15mmcq">
+        <div className="button-a button">
+          <div className="a-button-background" />
+          <div className="a-text">A</div>
+        </div>
+        <div className="button-b button">
+          <div className="b-button-background" />
+          <div className="b-text">B</div>
+        </div>
+        <div className="button-c button">
+          <div className="c-button-background" />
+          <div className="c-text">C</div>
+        </div>
+        <div className="button-d button">
+          <div className="c-button-background" />
+          <div className="d-text">D</div>
+        </div>
+        <div className="button-e">
+          <div className="e-button-background" />
+          <div className="e-text">E</div>
+        </div>
+        <div className="savenext-button">
+          <div className="savenextbuttonbackground" />
+          <div className="savenext-text">{`Save & Next`}</div>
+        </div>
+        <section className="desktop-15mmcq-child" />
+        <div className="button-4">
+          <div className="button-background" />
+          <div className="text">4</div>
+        </div>
+        <div className="button-5">
+          <div className="button-background" />
+          <div className="text">5</div>
+        </div>
+        <div className="button-6">
+          <div className="button-background" />
+          <div className="text2">6</div>
+        </div>
+        <div className="button-8">
+          <div className="button-background" />
+          <div className="text2">8</div>
+        </div>
+        <div className="button-9">
+          <div className="button-background" />
+          <div className="text4">9</div>
+        </div>
+        <div className="button-10">
+          <div className="button-background" />
+          <div className="text5">10</div>
+        </div>
+        <div className="button-7">
+          <img
+            className="button-background-icon"
+            alt=""
+            src="/7-button-background.svg"
+          />
+          <div className="text6">7</div>
+        </div>
+        <div className="button-3">
+          <div className="button-background" />
+          <div className="text7">3</div>
+        </div>
+        <div className="button-1">
+          <div className="button-background" />
+          <div className="text8">1</div>
+        </div>
+        <div className="button-11">
+          <div className="button-background" />
+          <div className="text9">11</div>
+        </div>
+        <div className="button-12">
+          <div className="button-background9" />
+          <div className="text10">12</div>
+        </div>
+        <div className="button-13">
+          <div className="button-background9" />
+          <div className="text10">13</div>
+        </div>
+        <div className="button-14">
+          <div className="button-background9" />
+          <div className="text12">14</div>
+        </div>
+        <div className="button-15">
+          <div className="button-background" />
+          <div className="text13">15</div>
+        </div>
+        <div className="button-16">
+          <div className="button-background" />
+          <div className="text13">16</div>
+        </div>
+        <div className="button-17">
+          <div className="button-background9" />
+          <div className="text13">17</div>
+        </div>
+        <div className="button-18">
+          <div className="button-background9" />
+          <div className="text13">18</div>
+        </div>
+        <div className="button-19">
+          <div className="button-background" />
+          <div className="text17">19</div>
+        </div>
+        <div className="button-20">
+          <div className="button-background" />
+          <div className="text18">20</div>
+        </div>
+        <div className="button-2">
+          <div className="button-background" />
+          <div className="text19">2</div>
+        </div>
+        <div className="desktop-15mmcq-item" />
+        <div className="timernet">
+          <div className="timer">
+            <div className="timer1" />
+            <div className="inside-timer" />
+            <div className="div">24:00</div>
+          </div>
+          <div className="timernet-child" />
+          <div className="timernet-item" />
+        </div>
+      </div>
+      <div className="iphone-14-1-5mmcq">
+        <div className="button-a1">
+          <div className="a-button-background" />
+          <div className="a-text">A</div>
+        </div>
+        <div className="button-b1">
+          <div className="b-button-background" />
+          <div className="b-text">B</div>
+        </div>
+        <div className="button-c1">
+          <div className="c-button-background" />
+          <div className="c-text">C</div>
+        </div>
+        <div className="button-d1">
+          <div className="c-button-background" />
+          <div className="d-text">D</div>
+        </div>
+        <div className="button-e1">
+          <div className="e-button-background" />
+          <div className="e-text">E</div>
+        </div>
+        <div className="savenext-button1">
+          <div className="savenextbuttonbackground" />
+          <div className="savenext-text">{`Save & Next`}</div>
+        </div>
+        <div className="iphone-14-1-5mmcq-child" />
+        <div className="button-51">
+          <div className="button-background" />
+          <div className="text">5</div>
+        </div>
+        <div className="button-41">
+          <div className="button-background" />
+          <div className="text">4</div>
+        </div>
+        <div className="button-31">
+          <div className="button-background" />
+          <div className="text7">3</div>
+        </div>
+        <div className="button-110">
+          <div className="button-background" />
+          <div className="text8">1</div>
+        </div>
+        <div className="button-101">
+          <div className="button-background" />
+          <div className="text5">10</div>
+        </div>
+        <div className="button-91">
+          <div className="button-background" />
+          <div className="text4">9</div>
+        </div>
+        <div className="button-81">
+          <div className="button-background" />
+          <div className="text2">8</div>
+        </div>
+        <div className="button-61">
+          <div className="button-background" />
+          <div className="text2">6</div>
+        </div>
+        <div className="button-111">
+          <div className="button-background" />
+          <div className="text9">11</div>
+        </div>
+        <div className="button-121">
+          <div className="button-background9" />
+          <div className="text10">12</div>
+        </div>
+        <div className="button-131">
+          <div className="button-background9" />
+          <div className="text10">13</div>
+        </div>
+        <div className="button-141">
+          <div className="button-background9" />
+          <div className="text12">14</div>
+        </div>
+        <div className="button-151">
+          <div className="button-background" />
+          <div className="text13">15</div>
+        </div>
+        <div className="button-161">
+          <div className="button-background" />
+          <div className="text13">16</div>
+        </div>
+        <div className="button-171">
+          <div className="button-background9" />
+          <div className="text13">17</div>
+        </div>
+        <div className="button-181">
+          <div className="button-background9" />
+          <div className="text13">18</div>
+        </div>
+        <div className="button-191">
+          <div className="button-background" />
+          <div className="text17">19</div>
+        </div>
+        <div className="button-201">
+          <div className="button-background" />
+          <div className="text18">20</div>
+        </div>
+        <div className="button-71">
+          <img
+            className="button-background-icon"
+            alt=""
+            src="/7-button-background.svg"
+          />
+          <div className="text6">7</div>
+        </div>
+        <div className="button-21">
+          <div className="button-background" />
+          <div className="text19">2</div>
+        </div>
+        <div className="timernet1">
+          <div className="timer">
+            <div className="timer1" />
+            <div className="inside-timer" />
+            <div className="div">24:00</div>
+          </div>
+          <div className="timernet-child" />
+          <div className="timernet-item" />
+        </div>
+      </div>
+      <div className="ipad-pro-11-1-5mmcq">
+        <div className="button-a2">
+          <div className="a-button-background" />
+          <div className="a-text">A</div>
+        </div>
+        <div className="button-b2">
+          <div className="b-button-background" />
+          <div className="b-text">B</div>
+        </div>
+        <div className="button-c2">
+          <div className="c-button-background" />
+          <div className="c-text">C</div>
+        </div>
+        <div className="button-d2">
+          <div className="c-button-background" />
+          <div className="d-text">D</div>
+        </div>
+        <div className="button-e2">
+          <div className="e-button-background" />
+          <div className="e-text">E</div>
+        </div>
+        <div className="savenext-button2">
+          <div className="savenextbuttonbackground" />
+          <div className="savenext-text">{`Save & Next`}</div>
+        </div>
+        <div className="ipad-pro-11-1-5mmcq-child" />
+        <div className="button-42">
+          <div className="button-background" />
+          <div className="text">4</div>
+        </div>
+        <div className="button-52">
+          <div className="button-background" />
+          <div className="text">5</div>
+        </div>
+        <div className="button-62">
+          <div className="button-background" />
+          <div className="text2">6</div>
+        </div>
+        <div className="button-82">
+          <div className="button-background" />
+          <div className="text2">8</div>
+        </div>
+        <div className="button-92">
+          <div className="button-background" />
+          <div className="text4">9</div>
+        </div>
+        <div className="button-102">
+          <div className="button-background" />
+          <div className="text5">10</div>
+        </div>
+        <div className="button-72">
+          <img
+            className="button-background-icon"
+            alt=""
+            src="/7-button-background.svg"
+          />
+          <div className="text6">7</div>
+        </div>
+        <div className="button-32">
+          <div className="button-background" />
+          <div className="text7">3</div>
+        </div>
+        <div className="button-112">
+          <div className="button-background" />
+          <div className="text8">1</div>
+        </div>
+        <div className="button-113">
+          <div className="button-background" />
+          <div className="text9">11</div>
+        </div>
+        <div className="button-122">
+          <div className="button-background9" />
+          <div className="text10">12</div>
+        </div>
+        <div className="button-132">
+          <div className="button-background9" />
+          <div className="text10">13</div>
+        </div>
+        <div className="button-142">
+          <div className="button-background9" />
+          <div className="text12">14</div>
+        </div>
+        <div className="button-152">
+          <div className="button-background" />
+          <div className="text13">15</div>
+        </div>
+        <div className="button-162">
+          <div className="button-background" />
+          <div className="text13">16</div>
+        </div>
+        <div className="button-172">
+          <div className="button-background9" />
+          <div className="text13">17</div>
+        </div>
+        <div className="button-182">
+          <div className="button-background9" />
+          <div className="text13">18</div>
+        </div>
+        <div className="button-192">
+          <div className="button-background" />
+          <div className="text17">19</div>
+        </div>
+        <div className="button-202">
+          <div className="button-background" />
+          <div className="text18">20</div>
+        </div>
+        <div className="button-22">
+          <div className="button-background" />
+          <div className="text19">2</div>
+        </div>
+        <div className="timernet2">
+          <div className="timer">
+            <div className="timer1" />
+            <div className="inside-timer" />
+            <div className="div">24:00</div>
+          </div>
+          <div className="timernet-child" />
+          <div className="timernet-item" />
+        </div>
+        <div className="timernet3">
+          <div className="timer">
+            <div className="timer1" />
+            <div className="inside-timer" />
+            <div className="div">24:00</div>
+          </div>
+          <div className="timernet-child" />
+          <div className="timernet-item" />
+        </div>
+      </div>
+      <div className="button-a3">
+        <div className="button-a-child" />
+        <div className="a">A</div>
+      </div>
+      <div className="button-b3">
+        <div className="button-b-child" />
+        <div className="a">B</div>
+      </div>
+      <div className="button-c3">
+        <div className="button-c-child" />
+        <div className="c">C</div>
+      </div>
+      <div className="savenextbutton">
+        <div className="savenextbutton-child" />
+        <div className="save-next">{`Save & Next`}</div>
+      </div>
+      <div className="button-114">
+        <div className="button-1-child" />
+        <div className="div4">1</div>
+      </div>
+      <div className="button-23">
+        <div className="button-1-child" />
+        <div className="div5">2</div>
+      </div>
+      <div className="button-33">
+        <div className="button-1-child" />
+        <div className="div5">3</div>
+      </div>
+      <div className="button-63">
+        <div className="button-6-child" />
+        <div className="div7">6</div>
+      </div>
+      <div className="button-73">
+        <div className="button-6-child" />
+        <div className="div7">7</div>
+      </div>
+      <div className="button-83">
+        <div className="button-6-child" />
+        <div className="div7">8</div>
+      </div>
+      <div className="button-93">
+        <div className="button-6-child" />
+        <div className="div7">9</div>
+      </div>
+      <div className="button-103">
+        <div className="button-1-child" />
+        <div className="div11">10</div>
+      </div>
+      <div className="button-203">
+        <div className="button-1-child" />
+        <div className="div12">20</div>
+      </div>
+      <div className="button-193">
+        <div className="button-1-child" />
+        <div className="div13">19</div>
+      </div>
+      <div className="button-183">
+        <div className="button-1-child" />
+        <div className="div13">18</div>
+      </div>
+      <div className="button-173">
+        <div className="button-6-child" />
+        <div className="div13">17</div>
+      </div>
+      <div className="button-163">
+        <div className="button-6-child" />
+        <div className="div13">16</div>
+      </div>
+      <div className="button-153">
+        <img className="button-15-child" alt="" src="/rectangle-6.svg" />
+        <div className="div17">15</div>
+      </div>
+      <div className="button-143">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div18">14</div>
+      </div>
+      <div className="button-133">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div18">13</div>
+      </div>
+      <div className="button-123">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div18">12</div>
+      </div>
+      <div className="button-115">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div21">11</div>
+      </div>
+      <div className="button-53">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div7">5</div>
+      </div>
+      <div className="button-43">
+        <img className="button-14-child" alt="" src="/rectangle-61.svg" />
+        <div className="div7">4</div>
+      </div>
+      <div className="timernet4">
+        <div className="timer">
+          <div className="timer1" />
+          <div className="inside-timer" />
+          <div className="div">24:00</div>
+        </div>
+        <div className="timernet-child" />
+        <div className="timernet-item" />
+      </div>
+    </div>
+  );
+};
+
+export default Desktop23mmcq;
